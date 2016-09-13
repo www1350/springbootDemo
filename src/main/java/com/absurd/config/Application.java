cc29c1e5c72e48ec38e43699d3ec481daf496dbd
www1350 <www_1350@163.com>
2016-09-13 19:53:46 +0800

    @add maven

--- src/main/java/com/absurd/config/Application.java
+++ src/main/java/com/absurd/config/Application.java
@@ -1,12 +1,5 @@
 package com.absurd.config;
 
-import com.absurd.controller.IndexController;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +7,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.*;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import javax.sql.DataSource;
 
